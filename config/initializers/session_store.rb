482848ed02b20226f93c49471a8ad85e250485fe
Ryan Jones <ryan@system88.com>
2012-10-11 14:21:58 -0600

    1.9 sntax

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TestRepo::Application.config.session_store :cookie_store, :key => '_test-repo_session'
+TestRepo::Application.config.session_store :cookie_store, key: '_test-repo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
